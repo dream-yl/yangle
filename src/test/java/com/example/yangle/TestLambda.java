303b8c3e0f9728f9e641c9b65797ef8c44a9c2b6
dream-yl <y15617832127@163.com>
2020-06-24 16:07:54 +0800

    项目

---
+++ src/test/java/com/example/yangle/TestLambda.java
@@ -0,0 +1,14 @@
+package com.example.yangle;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestLambda {
+    List<Employee> employees = Arrays.asList(
+            new Employee("张三",50,9999.99),
+            new Employee("李四",38,4444.44),
+            new Employee("王五",18,6666.66),
+            new Employee("赵六",16,7777.77),
+            new Employee("田七",28,8888.88)
+    );
+}
